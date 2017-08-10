61e6e24edbc393ae42702a60e285484f7c547531
Josh Harrington <josh@joshharrington.co.uk>
2017-08-10 17:46:48 +0100

    tidy up nav styling

--- js/big_nav.js
+++ js/big_nav.js
@@ -1,5 +1,7 @@
 var bigNav = document.getElementsByClassName('big-nav')[0];
 
+var navWrapper = document.getElementsByClassName('nav-wrapper')[0];
+
 var firstLevelNav = document.getElementsByClassName('first-level')[0];
 var firstLevelItems = firstLevelNav.getElementsByTagName('li');
 var firstLevelItemsSlicey = Array.prototype.slice.call(firstLevelItems);
@@ -10,32 +12,14 @@ var secondLevelItems = secondLevelNav.getElementsByTagName('ul');
 var defaultBlock = document.getElementsByClassName('default-block')[0];
 
 firstLevelNav.addEventListener('mouseover', listHoverAction, false);
-document.addEventListener('click', resetDefaultBlock, false);
+navWrapper.addEventListener('mouseleave', resetDefaultBlock, false);
+document.addEventListener('click', resetDefaultBlockTargetCheck, false);
 
 function listHoverAction(e) {
-	var target;
-
-	// if (e.target.tagName.toLowerCase() !== 'li') {
-	// 	target = e.target.closest('li');
-	// 	var index = firstLevelItemsSlicey.indexOf(e.target);
-	// 	var listBgImgData = target.dataset.bgImg;
-
-
-	// 	clearSelectedAndShowy();
-	// 	hideDefaultNavMsg();
-
-	// 	bigNav.dataset.bgImg = listBgImgData; 
-
-	// 	target.classList.add('selected');
-	// 	secondLevelItems[index].classList.add('show');
-
-	// 	e.stopPropagation();
-	// } else 
 	if (e.target.tagName.toLowerCase() === 'li') {
-		target = e.target;
+		var target = e.target;
 		var index = firstLevelItemsSlicey.indexOf(e.target);
 		var listBgImgData = target.dataset.bgImg;
-
 
 		clearSelectedAndShowy();
 		hideDefaultNavMsg();
@@ -56,12 +40,18 @@ function hideDefaultNavMsg(e) {
 	}
 };
 
-function resetDefaultBlock(e) {
+function resetDefaultBlockTargetCheck(e) {
 	if (e.target !== firstLevelNav && !firstLevelNav.contains(e.target) && e.target !== secondLevelNav && !secondLevelNav.contains(e.target)) {
 		clearSelectedAndShowy();
 		defaultBlock.classList.remove('hide');
 		bigNav.dataset.bgImg = 'default';
 	}
+}
+
+function resetDefaultBlock() {
+	clearSelectedAndShowy();
+	defaultBlock.classList.remove('hide');
+	bigNav.dataset.bgImg = 'default';
 }
 
 function clearSelectedAndShowy(e) {
